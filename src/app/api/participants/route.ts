667a6de855b67ba5d770d681af7ab52d025c97b3
Carl Jeffrie Panilag <cjpanilag@gmail.com>
2024-11-09 11:44:09 +0800

    :boom: Updated participants
    - Added Update Participant API
    - Updated SQL schema

--- src/app/api/participants/route.ts
+++ src/app/api/participants/route.ts
@@ -1,11 +1,34 @@
 import { DatabaseError } from "@/definitions";
 import { query } from "@/lib/db";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET() {
   try {
-    const result = await query("SELECT name, email FROM participants");
+    const result = await query(
+      "SELECT id, name, email, created_at FROM participants WHERE status=1 ORDER BY created_at ASC",
+    );
     return NextResponse.json(result.rows);
+  } catch (err: unknown) {
+    if (isDatabaseError(err) && err.code === "23505") {
+      return NextResponse.json({ error: err.code }, { status: 409 });
+    } else {
+      return NextResponse.json(
+        { error: "Something went wrong" },
+        { status: 500 },
+      );
+    }
+  }
+}
+
+export async function PUT(req: NextRequest) {
+  const data = await req.json();
+  const { id, status } = data;
+  try {
+    const result = await query(
+      "UPDATE participants SET status = $1 WHERE id = $2 RETURNING *",
+      [status, id],
+    );
+    return NextResponse.json(result.rows[0]);
   } catch (err: unknown) {
     if (isDatabaseError(err) && err.code === "23505") {
       return NextResponse.json({ error: err.code }, { status: 409 });
